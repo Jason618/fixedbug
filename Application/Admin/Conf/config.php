d9ddde0bae74e788c6f02f713d757af7ac7dcdce
lichengjun <lichengjun@lichengjuns-MacBook-Pro.local>
2016-11-17 17:25:27 +0800

    article detail

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,5 +1,9 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-
+    //路由配置
+    'URL_ROUTER_ON'   => true,
+    'URL_ROUTE_RULES'=>array(
+        'Article/j/:uuid' => 'Article/detail'
+    )
 );
--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -26,8 +26,16 @@ class ArticleController extends Controller
         $Page = new \Think\Page($count, 2);// 实例化分页类 传入总记录数和每页显示的记录数(2)
         $show = $Page->show();// 分页显示输出
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性   按文章加入时间倒序
-        $list = $Articles->table('__ARTICLE__ as a')->field('a.id,a.title,a.content,u.nickname,u.face,c.name as category_name')->join('__USER__ as u ON a.user_id = u.id')->join('__CATEGORY__ as c ON a.category_id = c.id')->order('a.create_time desc')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+        $list = $Articles->table('__ARTICLE__ as a')->field('a.id,a.uuid,a.title,a.content,u.nickname,u.face,c.name as category_name')->join('__USER__ as u ON a.user_id = u.id')->join('__CATEGORY__ as c ON a.category_id = c.id')->order('a.create_time desc')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+
+        /*$newList = array();
+        foreach($list as $item){
+            $item['uuid'] = md5($item['id'].'jason');    //文章详情UUID 又id+jason MD5
+            array_push($newList,$item);
+        }
+        dump($newList);*/
         $this->assign('list', $list);// 赋值数据集
+
         $this->assign('page', $show);// 赋值分页输出
         $this->display(); // 输出模板
     }
@@ -54,9 +62,22 @@ class ArticleController extends Controller
         $data['title'] = $title;
         $data['content'] = $content;
         $data['category_id'] = $categoryId;
+        $data['uuid'] = substr(md5(time().'Jason'),0,20);  //文章uuid
 
         $Article->add($data);
         $this->success('新增成功', 'index');  //页面跳转
 
+    }
+
+    //文章详情
+    public function detail(){
+        $uuid = $_GET['uuid'];
+        $Article = M('article');
+        //$Article
+        $data =$Article->table('__ARTICLE__ as a')->field('a.id,a.uuid,a.title,a.content,u.nickname,u.face,c.name as category_name')->join('__USER__ as u ON a.user_id = u.id')->join('__CATEGORY__ as c ON a.category_id = c.id')->where('a.uuid="'.$uuid.'"')->find();
+
+        dump($data);
+        $this->assign('article',$data);
+        $this->display(); // 输出模板
     }
 }
